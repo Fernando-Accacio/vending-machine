af178955e00b6c93649b2f34ad6b4cf2ce4ee025
Fernando-Accacio <fernando.accacio15@gmail.com>
2025-11-08 22:12:25 -0300

    /

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // O seu back-end de produção
-  apiUrl: 'https://vending-machine-z87w.onrender.com' 
+  apiUrl: 'https://vending-machine-z87w.onrender.com/' 
 };
