0ceefe009ce79290cec2c2a56ee25adc211194a5
Fernando-Accacio <fernando.accacio15@gmail.com>
2025-11-09 22:21:56 -0300

    Buscando ajuste de horário

--- frontend/src/app/pages/auth/login/login.component.ts
+++ frontend/src/app/pages/auth/login/login.component.ts
@@ -5,70 +5,57 @@ import { AuthenticateService } from '../../../services/auth/authenticate.service
 import { Router, RouterModule } from '@angular/router'; 
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [
-    FormsModule, 
-    CommonModule, 
-    RouterModule 
-  ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    FormsModule, 
+    CommonModule, 
+    RouterModule 
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  user = {
-    documento: '',
-    password: ''
-  }
+  user = {
+    documento: '',
+    password: ''
+  }
 
-  constructor(private authService: AuthenticateService, private router: Router) {
-    console.log('LoginComponent carregado.'); // 1. O componente foi carregado?
-  }
+  constructor(private authService: AuthenticateService, private router: Router) {
+  }
 
-  login() {
-    console.log('--- LOGIN CLICADO ---'); // 2. O clique no botão foi detectado?
-    
-    this.authService.login(this.user.documento, this.user.password || '').subscribe(
-      (data) => {
-        console.log('SUCESSO NA REQUISIÇÃO! Dados recebidos:', data); // 3. O backend respondeu com sucesso?
-        
-        this.authService.saveToken(data.token);
-        const role = this.authService.getRole(); 
-        
-        console.log('SUCESSO NO LOGIN! Role extraída do token:', role); // 4. A 'role' foi lida?
-        
-        const cart = localStorage.getItem('cart');
-        if (cart) {
-          console.log('Carrinho encontrado, redirecionando para /');
-          this.router.navigate(['/']); 
-          return;
-        }
-        
-        switch(role) {
-          case 'GERENTE':
-            console.log('Redirecionando para /itens');
-            this.router.navigate(['/itens']); 
-            break;
-          case 'cliente':
-            console.log('Redirecionando para / (cliente)');
-            this.router.navigate(['/']);
-            break
-          case 'entregador':
-            console.log('Redirecionando para /entregas');
-            this.router.navigate(['/entregas']); 
-            break
-          default:
-            console.log('Role não bateu ("' + role + '"), redirecionando para / (default)');
-            this.router.navigate(['/']);
-        }
-      },
-      (error) => {
-        // 5. Se o login falhar (senha errada), deve cair aqui.
-        alert('Documento e/ou senha inválidas!') 
-        console.error('ERRO DE AUTENTICAÇÃO:', error);
-      }
-    );
-    
-    console.log('Função login() terminada.'); // 6. A função foi executada até o fim?
-  }
+  login() {
+    
+    this.authService.login(this.user.documento, this.user.password || '').subscribe(
+      (data) => {
+        
+        this.authService.saveToken(data.token);
+        const role = this.authService.getRole(); 
+        
+        const cart = localStorage.getItem('cart');
+        if (cart) {
+          this.router.navigate(['/']); 
+          return;
+        }
+        
+        switch(role) {
+          case 'GERENTE':
+            this.router.navigate(['/itens']); 
+            break;
+          case 'cliente':
+            this.router.navigate(['/']);
+            break
+          case 'entregador':
+            this.router.navigate(['/entregas']); 
+            break
+          default:
+            this.router.navigate(['/']);
+        }
+      },
+      (error) => {
+        alert('Documento e/ou senha inválidas!') 
+        console.error('ERRO DE AUTENTICAÇÃO:', error);
+      }
+    );
+  }
 }
--- frontend/src/app/pages/cliente/my-withdrawals/my-withdrawals.component.ts
+++ frontend/src/app/pages/cliente/my-withdrawals/my-withdrawals.component.ts
@@ -49,3 +49,4 @@ export class MyWithdrawalsComponent implements OnInit {
     });
   }
 }
+
